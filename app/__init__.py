aef7d84d9e9bf864e59809c9997f8b77fdd83782
John-Kimani <kimwaweru100@gmail.com>
2022-02-22 22:20:08 +0300

    imports devConfig

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,11 @@
-from flask import Flask #import flask from flask module
+from flask import Flask
+
+from app.config import DevConfig #import flask from flask module
 
 # Initializing application
 app = Flask(__name__) #create app instance
+
+# Setting up configuration
+app.config.from_object(DevConfig)
 
 from app import views
