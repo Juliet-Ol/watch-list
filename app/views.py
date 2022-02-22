f3aecdb0ed909ae9cb07967e285f0aa1edc2f033
John-Kimani <kimwaweru100@gmail.com>
2022-02-22 22:02:42 +0300

    creates a dynamic route

--- app/views.py
+++ app/views.py
@@ -9,3 +9,10 @@ def index():
     '''
     message = 'Hello world' #new variable
     return render_template('index.html', message = message)
+
+@app.route('/movie/<int:movie_id>')
+def movie(movie_id):
+    '''
+    View movie page function that returns the movie detail page and its data
+    '''
+    return render_template('movie.html', id = movie_id)
