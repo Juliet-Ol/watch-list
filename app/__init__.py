82e9d19fb29a1b86d4d2b0fd7f4300d98e6fea07
John-Kimani <kimwaweru100@gmail.com>
2022-02-22 22:51:48 +0300

    fix config import error

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 
 from app.config import DevConfig #import flask from flask module
 
+from .config import DevConfig
+
 # Initializing application
 app = Flask(__name__) #create app instance
 
