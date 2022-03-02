beb563977cdfefb16370f45b6f60c660a6568f53
John-Kimani <kimwaweru100@gmail.com>
2022-03-02 10:47:14 +0300

    Dev: Restructure Config file

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 from instance.config import MOVIE_API_KEY
-
+import os
 
 class Config:
     '''
@@ -7,6 +7,8 @@ class Config:
     '''
     # MOVIE_API_KEY = 'https://api.themoviedb.org/3/movie{}?api_key={}' #
     MOVIE_API_BASE_URL = 'https://api.themoviedb.org/3/movie/{}?api_key={}'
+    MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
 class ProdConfig(Config):
     '''
@@ -24,4 +26,9 @@ class DevConfig(Config):
     Config: The parent configuration class with General configuration settings
     '''
     DEBUG = True
+
+config_options = {
+    'development':DevConfig,
+    'production':ProdConfig
+}
     
