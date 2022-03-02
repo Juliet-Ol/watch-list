38c93b37918b06c6e66fc0d1866c83a57df32b8a
John-Kimani <kimwaweru100@gmail.com>
2022-03-02 11:46:20 +0300

    Dev: Create test folders deletes movie_test file from app folder

---
+++ tests/test_movie.py
@@ -0,0 +1,17 @@
+import unittest
+from app.models import Movie
+
+
+class MovieTest(unittest.TestCase):
+    '''
+    Tesy class to test the behaviour of the movie class
+    '''
+    def setUp(self):
+        '''
+        Set up method that will ru before every test
+        '''
+        self.new_movie = Movie(1234, 'Python Must be Crazy', 'A thrilling new python series', 'https://image.tmdb.org/t/p/w500/khsjha27hbs',8.5,129993)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_movie,Movie))
+
